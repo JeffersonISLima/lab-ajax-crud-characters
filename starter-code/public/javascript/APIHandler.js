a0a286d5e68e2a91b10591dfe22f602a329acb00
Heitor Godau <heitorgodau@gmail.com>
2019-04-25 20:22:07 -0300

    "FInd All", "Find One" and "Delete" Done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,36 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    return axios.get(`${this.BASE_URL}/characters`);
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
   }
 
   createOneRegister () {
-
+    axios.post(`${this.BASE_URL}/characters`)
+      .then(result => result.data)
+      .catch(err => console.log(err));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, charValues) {
+    //axios.put(`${this.BASE_URL}/characters/${id}`, charValues);
+    console.log(id, charValues);
+    axios.patch(`${this.BASE_URL}/characters/${id}`)
+      .then(() => {
+        console.log('Criado com sucesso');        
+        document.getElementById('edit-character-form').reset();        
+      })
+      .catch((err) => {
+        console.log(err);        
+      })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
   }
 }
